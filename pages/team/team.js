4f13f3fa8804d541ed6897a95dc998cf97f0c644
kerwin <kerwinwoo@gmail.com>
2019-04-13 23:54:39 +0800

    增加步友团页面

---
+++ pages/team/team.js
@@ -0,0 +1,82 @@
+// pages/team/team.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    array: [5000, 10000, 15000, 20000],
+    index: 0
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  backTo (e) {
+    wx.switchTab({
+    	url: '/pages/index/index'
+    })
+  },
+  bindPickerChange(e) {
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  createTeam () {
+    wx.navigateTo({
+      url: '/pages/team/myteam/myteam'
+    })
+  }
+})
